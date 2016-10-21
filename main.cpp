87037dc8e8086a927b42a5de281a9e15c43844d2
Boris Sivko <bsivko@gmail.com>
2016-10-21 12:55:57 +0300

    moved to func

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 
 // Comment - simple calculation.
 
-int main() {
-    
+int func() {
     int x = 0;
-    for(int i = 0; i < 10; ++i)
-        x += i*i-2*i;
-    
-    std::cout << "x:" << x << std::endl;
+    for (int i = 0; i < 10; ++i)
+        x += i * i - 2 * i;
+
+    return x;
+}
+
+int main() {
+    std::cout << "x:" << func() << std::endl;
     return 0;
 }
