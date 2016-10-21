8a60eef7c242006284704b6c50368be7a25db148
Boris Sivko <bsivko@gmail.com>
2016-10-21 16:37:12 +0300

    tested function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 // Comment - simple calculation.
 
+bool test_func() {
+    if (func() > 0)
+        return true;
+    return false;
+}
+
 int func() {
     int x = 0;
     for (int i = 0; i < 10; ++i)
@@ -14,5 +20,6 @@ int func() {
 
 int main() {
     std::cout << "x:" << func() << ";" << std::endl;
+    std::cout << "test:" << test_func() << ";" << std::endl;
     return 0;
 }
